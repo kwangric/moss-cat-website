941d5e37a6209308a5923b8e78d972aa74e23b54
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2025-12-02 20:55:34 +0000

    Convert site to Eleventy and add Pages workflow

---
+++ eleventy.config.cjs
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("styles.css")
+    eleventyConfig.addPassthroughCopy("script.js")
+    
+    eleventyConfig.addPassthroughCopy("CNAME")
+    eleventyConfig.addPassthroughCopy("favicon.ico")
+    eleventyConfig.addPassthroughCopy("og-image.png")
+    eleventyConfig.addPassthroughCopy("robots.txt")
+
+    return {
+        dir: {
+        input: ".",
+        output: "_site"
+        }
+    }
+}
